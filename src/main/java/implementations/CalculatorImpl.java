b7df4303fc7d1886ae5ba5a85cae2178099ea436
AAgustin9 <aalma@mail.austral.edu.ar>
2023-04-10 20:40:48 -0300

    -finished binHex and hexBin with their tests.

--- src/main/java/implementations/CalculatorImpl.java
+++ src/main/java/implementations/CalculatorImpl.java
@@ -2,6 +2,11 @@ package implementations;
 
 import interfaces.Calculator;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class CalculatorImpl implements Calculator {
 
     private String order(String a, String b) {
@@ -70,14 +75,83 @@ public class CalculatorImpl implements Calculator {
         return null;
     }
 
+    HashMap<String, String> binMap = new HashMap<>() {{
+            put("0000", "0");
+            put("0001", "1");
+            put("0010", "2");
+            put("0011", "3");
+            put("0100", "4");
+            put("0101", "5");
+            put("0110", "6");
+            put("0111", "7");
+            put("1000", "8");
+            put("1001", "9");
+            put("1010", "A");
+            put("1011", "B");
+            put("1100", "C");
+            put("1101", "D");
+            put("1110", "E");
+            put("1111", "F");
+        }};
+
     @Override
     public String toHex(String binary) {
-        return null;
+        String hexa = "";
+        String ceros = "";
+        List<String> separateList = new ArrayList<>();
+        String orderedBinary = "";
+        //Checks that the number can be separated in 4's.
+        if (binary.length() % 4 !=0){
+            int adders = 4 - binary.length() % 4; //finds the missing 0's.
+            for (int i = 0; i < adders; i++) {
+                ceros += "0";
+            }
+            orderedBinary = ceros + binary;
+        }
+        for (int i = 0; i < orderedBinary.length(); i+=4) {
+            separateList.add(orderedBinary.substring(i, i+4));
+        }
+
+        for (String slice: separateList) {
+
+            for (String key : binMap.keySet()) {
+                if (key.equals(slice)) {
+                    hexa += binMap.get(slice);
+                }
+            }
+        }
+
+        return hexa;
     }
+
+    HashMap<Character, String> hexMap = new HashMap<>() {{
+        put('0', "0000");
+        put('1', "0001");
+        put('2', "0010");
+        put('3', "0011");
+        put('4', "0100");
+        put('5', "0101");
+        put('6', "0110");
+        put('7', "0111");
+        put('8', "1000");
+        put('9', "1001");
+        put('A', "1010");
+        put('B', "1011");
+        put('C', "1100");
+        put('D', "1101");
+        put('E', "1110");
+        put('F', "1111");
+    }};
 
     @Override
     public String fromHex(String hex) {
-        return null;
+        String bin = "";
+        for (int i = 0; i < hex.length(); i++) {
+            if (hexMap.containsKey(hex.charAt(i))){
+                bin += hexMap.get(hex.charAt(i));
+            }
+        }
+        return bin;
     }
 
     private String complement(String a) {
